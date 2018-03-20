9f0db429d84565cb339379392051afaedf8e79d5
Adam Allport <adam@allport.me.uk>
2018-03-20 15:38:40 +0000

    Created the datetime app. Needs finishing but i was busy showing ppl on the projector

---
+++ 01-DateTime-VB/01-DateTime-VB/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        Dim today As Date
+        Dim todayTime As DateTime
+        Dim time As TimeSpan
+        today = DateAndTime.Now.Date
+        todayTime = DateAndTime.Now.ToString("HH:mm:s")
+
+        lblOutput.Text = "Today's date is: " + today
+    End Sub
+End Class
---
+++ 01-DateTime-VB/01-DateTime-VB/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("01-DateTime-VB")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("01-DateTime-VB")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("039af6d7-84c5-4ead-a585-dcab33afeccd")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
