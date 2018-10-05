22d43efa2536889dfe75ed87d17d7b1888dabd17
Adam Allport <adam@allport.me.uk>
2018-10-05 12:11:18 +0100

    Added cpp

---
+++ CPlusPlus/E-01-HelloWorld-CPP/cmake-build-debug/CMakeFiles/E_01_HelloWorld_CPP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/E_01_HelloWorld_CPP.dir/main.cpp.obj"
+  "E_01_HelloWorld_CPP.pdb"
+  "E_01_HelloWorld_CPP.exe"
+  "E_01_HelloWorld_CPP.exe.manifest"
+  "libE_01_HelloWorld_CPP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/E_01_HelloWorld_CPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
